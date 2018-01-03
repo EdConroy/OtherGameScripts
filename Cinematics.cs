7cfea2ce7d44f4cc30c9feed9aac13874dd3f3b1
Ed Conroy <edconroy5@gmail.com>
2018-01-03 00:50:14 -0500

    Add files via upload

---
+++ Cinematics.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cinematics : MonoBehaviour
+{
+    public AnimationClip clipName;
+    Animation ani;
+    public float distance;
+    public GameObject player;
+    public GameObject self;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        distance = Vector3.Distance(player.transform.position, self.transform.position);
+        
+        if(distance < 5.0f)
+        {
+            clipName.wrapMode = WrapMode.Loop;
+            ani.Play(clipName.name);
+            ani[clipName.name].speed = 0.1f;
+        }	
+	}
+}
---
+++ DPS.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DPS : Enemy
+{
+    public void Pursuit(UnityEngine.AI.NavMeshAgent agent, GameObject p_flank)
+    {
+        goal = p_flank.transform;
+        agent.destination = goal.position;
+    }
+    public void DPSAttack()
+    {
+        //Attack the player
+    }
+    public void DPSAnimate()
+    {
+        //Preform animations based on the current state the Enemy is in
+    }
+}
